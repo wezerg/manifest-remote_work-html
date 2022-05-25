222399c7b09c59706210847ab1c2ee46a4ec6114
wezerg <vivian.grimaux@hotmail.fr>
2022-05-25 19:02:58 +0200

    First template Premier template + import des polices d'écritures

--- assets/script.js
+++
@@ -1,7 +0,0 @@
-fetch('./includes/aside.html')
-    .then(function(response) {
-        return response.text();
-    })
-    .then(function(data) {
-        document.querySelector('nav').innerHTML = data;
-    });
