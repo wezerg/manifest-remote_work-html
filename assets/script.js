0df07ba9d14580fd00909c64c042288de9bf8a9a
wezerg <vivian.grimaux@hotmail.fr>
2022-05-26 20:03:22 +0200

    Ajout de sécurité sur les pramètres url

--- assets/script.js
+++ assets/script.js
@@ -2,10 +2,17 @@ function onLoadPage(){
     const slideToLoad = document.querySelector(`#${window.location.search.split('=')[1]}`);
     switch (window.location.pathname.split('/')[2]) {
         case "nouvelles-technologies.html":
-            scrollSlide(window.location.search.split('=')[1]);
+            if (window.location.search) {
+                scrollSlide(window.location.search.split('=')[1]);
+            }
             break;
         case "10-bonnes-raisons.html":
-            slideToLoad.classList.replace('d-none', 'd-block');
+            if (slideToLoad) {
+                slideToLoad.classList.replace('d-none', 'd-block');
+            }
+            else{
+                document.querySelector('#intro').classList.replace('d-none', 'd-block');
+            }
             break;
         default:
             break;
